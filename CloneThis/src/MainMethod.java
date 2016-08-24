aa474aa9d0010bed68213029bccdc871a89cee14
shuang106 <shuang106@S002-CNU346B58K.student.HISD.ORG>
2016-08-24 09:40:21 -0500

    I changed something

--- CloneThis/src/MainMethod.java
+++ CloneThis/src/MainMethod.java
@@ -4,8 +4,7 @@ public class MainMethod {
 	public static void main(String[] args) {
 
 		System.out.println("This is a test to make sure you can pull from a repo.");
-		System.out.println("Change this line to something else. Then commit & push to your repo.");
-
+		System.out.println("Math club tap sales are 1/23-1/27.");
 	}
 
 }
